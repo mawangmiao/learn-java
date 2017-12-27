ffa390245e66beafdcbc3d92bd6c9c4cbcb9de17
彭爽 <pengshuang@oneapm.com>
2017-12-27 14:56:27 +0800

    上传

--- src/main/java/com/example/btree/BTreeDepth.java
+++ src/main/java/com/example/btree/BTreeDepth.java
@@ -2,17 +2,38 @@ package com.example.btree;
 
 public class BTreeDepth {
 
-    public static <T extends Comparable<?>> int getDepth(BTreeNode<T> root) {
-        return postOrderTraverseInterval(root);
+    public static <T extends Comparable<?>> int getMaxDepth(BTreeNode<T> root) {
+        return postOrderTraverseToFindMaxDepth(root);
     }
 
-    private static <T extends Comparable<?>> int postOrderTraverseInterval(BTreeNode<T> root) {
+    private static <T extends Comparable<?>> int postOrderTraverseToFindMaxDepth(BTreeNode<T> root) {
         if (root == null) {
             return 0;
         }
 
-        int leftLength = postOrderTraverseInterval(root.left);
-        int rightLength = postOrderTraverseInterval(root.right);
+        int leftLength = postOrderTraverseToFindMaxDepth(root.left);
+        int rightLength = postOrderTraverseToFindMaxDepth(root.right);
         return leftLength > rightLength ? leftLength + 1 : rightLength + 1;
+    }
+
+    public static <T extends Comparable<?>> int getMinDepth(BTreeNode<T> root) {
+        if (root == null) {
+            return 0;
+        }
+        return postOrderTraverseToFindMinDepth(root);
+    }
+
+    private static <T extends Comparable<?>> int postOrderTraverseToFindMinDepth(BTreeNode<T> root) {
+        if (root == null) {//处理斜树
+            return Integer.MAX_VALUE;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int leftLength = postOrderTraverseToFindMinDepth(root.left);
+        int rightLength = postOrderTraverseToFindMinDepth(root.right);
+
+        return leftLength < rightLength ? leftLength + 1 : rightLength + 1;
     }
 }
